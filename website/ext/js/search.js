bd8ffe119ae80b1d7e444ce8db85abc576fc4bdb
Guillaume Claret <dev@clarus.me>
2014-07-21 14:19:01 +0200

    Compiled version of search.js added

--- website/ext/js/search.js
+++ website/ext/js/search.js
@@ -1,1 +1,219 @@
-../../src/search.js
+// This program was compiled from OCaml by js_of_ocaml 1.4
+function caml_raise_with_arg (tag, arg) { throw [0, tag, arg]; }
+function caml_raise_with_string (tag, msg) {
+  caml_raise_with_arg (tag, new MlWrappedString (msg));
+}
+function caml_invalid_argument (msg) {
+  caml_raise_with_string(caml_global_data[4], msg);
+}
+function caml_array_bound_error () {
+  caml_invalid_argument("index out of bounds");
+}
+function caml_str_repeat(n, s) {
+  if (!n) { return ""; }
+  if (n & 1) { return caml_str_repeat(n - 1, s) + s; }
+  var r = caml_str_repeat(n >> 1, s);
+  return r + r;
+}
+function MlString(param) {
+  if (param != null) {
+    this.bytes = this.fullBytes = param;
+    this.last = this.len = param.length;
+  }
+}
+MlString.prototype = {
+  string:null,
+  bytes:null,
+  fullBytes:null,
+  array:null,
+  len:null,
+  last:0,
+  toJsString:function() {
+    return this.string = decodeURIComponent (escape(this.getFullBytes()));
+  },
+  toBytes:function() {
+    if (this.string != null)
+      var b = unescape (encodeURIComponent (this.string));
+    else {
+      var b = "", a = this.array, l = a.length;
+      for (var i = 0; i < l; i ++) b += String.fromCharCode (a[i]);
+    }
+    this.bytes = this.fullBytes = b;
+    this.last = this.len = b.length;
+    return b;
+  },
+  getBytes:function() {
+    var b = this.bytes;
+    if (b == null) b = this.toBytes();
+    return b;
+  },
+  getFullBytes:function() {
+    var b = this.fullBytes;
+    if (b !== null) return b;
+    b = this.bytes;
+    if (b == null) b = this.toBytes ();
+    if (this.last < this.len) {
+      this.bytes = (b += caml_str_repeat(this.len - this.last, '\0'));
+      this.last = this.len;
+    }
+    this.fullBytes = b;
+    return b;
+  },
+  toArray:function() {
+    var b = this.bytes;
+    if (b == null) b = this.toBytes ();
+    var a = [], l = this.last;
+    for (var i = 0; i < l; i++) a[i] = b.charCodeAt(i);
+    for (l = this.len; i < l; i++) a[i] = 0;
+    this.string = this.bytes = this.fullBytes = null;
+    this.last = this.len;
+    this.array = a;
+    return a;
+  },
+  getArray:function() {
+    var a = this.array;
+    if (!a) a = this.toArray();
+    return a;
+  },
+  getLen:function() {
+    var len = this.len;
+    if (len !== null) return len;
+    this.toBytes();
+    return this.len;
+  },
+  toString:function() { var s = this.string; return s?s:this.toJsString(); },
+  valueOf:function() { var s = this.string; return s?s:this.toJsString(); },
+  blitToArray:function(i1, a2, i2, l) {
+    var a1 = this.array;
+    if (a1) {
+      if (i2 <= i1) {
+        for (var i = 0; i < l; i++) a2[i2 + i] = a1[i1 + i];
+      } else {
+        for (var i = l - 1; i >= 0; i--) a2[i2 + i] = a1[i1 + i];
+      }
+    } else {
+      var b = this.bytes;
+      if (b == null) b = this.toBytes();
+      var l1 = this.last - i1;
+      if (l <= l1)
+        for (var i = 0; i < l; i++) a2 [i2 + i] = b.charCodeAt(i1 + i);
+      else {
+        for (var i = 0; i < l1; i++) a2 [i2 + i] = b.charCodeAt(i1 + i);
+        for (; i < l; i++) a2 [i2 + i] = 0;
+      }
+    }
+  },
+  get:function (i) {
+    var a = this.array;
+    if (a) return a[i];
+    var b = this.bytes;
+    if (b == null) b = this.toBytes();
+    return (i<this.last)?b.charCodeAt(i):0;
+  },
+  safeGet:function (i) {
+    if (this.len == null) this.toBytes();
+    if ((i < 0) || (i >= this.len)) caml_array_bound_error ();
+    return this.get(i);
+  },
+  set:function (i, c) {
+    var a = this.array;
+    if (!a) {
+      if (this.last == i) {
+        this.bytes += String.fromCharCode (c & 0xff);
+        this.last ++;
+        return 0;
+      }
+      a = this.toArray();
+    } else if (this.bytes != null) {
+      this.bytes = this.fullBytes = this.string = null;
+    }
+    a[i] = c & 0xff;
+    return 0;
+  },
+  safeSet:function (i, c) {
+    if (this.len == null) this.toBytes ();
+    if ((i < 0) || (i >= this.len)) caml_array_bound_error ();
+    this.set(i, c);
+  },
+  fill:function (ofs, len, c) {
+    if (ofs >= this.last && this.last && c == 0) return;
+    var a = this.array;
+    if (!a) a = this.toArray();
+    else if (this.bytes != null) {
+      this.bytes = this.fullBytes = this.string = null;
+    }
+    var l = ofs + len;
+    for (var i = ofs; i < l; i++) a[i] = c;
+  },
+  compare:function (s2) {
+    if (this.string != null && s2.string != null) {
+      if (this.string < s2.string) return -1;
+      if (this.string > s2.string) return 1;
+      return 0;
+    }
+    var b1 = this.getFullBytes ();
+    var b2 = s2.getFullBytes ();
+    if (b1 < b2) return -1;
+    if (b1 > b2) return 1;
+    return 0;
+  },
+  equal:function (s2) {
+    if (this.string != null && s2.string != null)
+      return this.string == s2.string;
+    return this.getFullBytes () == s2.getFullBytes ();
+  },
+  lessThan:function (s2) {
+    if (this.string != null && s2.string != null)
+      return this.string < s2.string;
+    return this.getFullBytes () < s2.getFullBytes ();
+  },
+  lessEqual:function (s2) {
+    if (this.string != null && s2.string != null)
+      return this.string <= s2.string;
+    return this.getFullBytes () <= s2.getFullBytes ();
+  }
+}
+function MlWrappedString (s) { this.string = s; }
+MlWrappedString.prototype = new MlString();
+function MlMakeString (l) { this.bytes = ""; this.len = l; }
+MlMakeString.prototype = new MlString ();
+function caml_create_string(len) {
+  if (len < 0) caml_invalid_argument("String.create");
+  return new MlMakeString(len);
+}
+function caml_js_from_byte_string (s) {return s.getFullBytes();}
+function caml_js_get_console () {
+  var c = this.console?this.console:{};
+  var m = ["log", "debug", "info", "warn", "error", "assert", "dir", "dirxml",
+           "trace", "group", "groupCollapsed", "groupEnd", "time", "timeEnd"];
+  function f () {}
+  for (var i = 0; i < m.length; i++) if (!c[m[i]]) c[m[i]]=f;
+  return c;
+}
+function caml_call_gen(f, args) {
+  if(f.fun)
+    return caml_call_gen(f.fun, args);
+  var n = f.length;
+  var d = n - args.length;
+  if (d == 0)
+    return f.apply(null, args);
+  else if (d < 0)
+    return caml_call_gen(f.apply(null, args.slice(0,n)), args.slice(n));
+  else
+    return function (x){ return caml_call_gen(f, args.concat([x])); };
+}
+function caml_js_wrap_callback(f) {
+  var toArray = Array.prototype.slice;
+  return function () {
+    var args = (arguments.length > 0)?toArray.call (arguments):[undefined];
+    return caml_call_gen(f, args);
+  }
+}
+function caml_ml_out_channels_list () { return 0; }
+var caml_global_data = [0];
+function caml_register_global (n, v) { caml_global_data[n + 1] = v; }
+var caml_named_values = {};
+function caml_register_named_value(nm,v) {
+  caml_named_values[nm] = v; return 0;
+}
+(function(){var a=[0,new MlString("Assert_failure")];caml_register_global(6,[0,new MlString("Not_found")]);caml_register_global(5,[0,new MlString("Division_by_zero")]);caml_register_global(3,[0,new MlString("Invalid_argument")]);caml_register_global(2,[0,new MlString("Failure")]);var s=new MlString("Pervasives.do_at_exit"),r=new MlString("table"),q=new MlString("input"),p=new MlString("g"),o=new MlString("g"),n=new MlString("[$]"),m=new MlString("[\\][()\\\\|+*.?{}^$]"),l=new MlString(""),k=new MlString("none"),j=[0,new MlString("search.ml"),28,12],i=[0,new MlString("search.ml"),24,45],h=new MlString("packages"),g=new MlString("search");function f(e){var b=caml_ml_out_channels_list(0);for(;;){if(b){var c=b[2];try {}catch(d){}var b=c;continue;}return 0;}}caml_register_named_value(s,f);function H(t){var u=t.getLen();if(0===u)var v=t;else{var w=caml_create_string(u),x=0,y=u-1|0;if(!(y<x)){var z=x;for(;;){var A=t.safeGet(z),B=65<=A?90<A?0:1:0;if(B)var C=0;else{if(192<=A&&!(214<A)){var C=0,D=0;}else var D=1;if(D){if(216<=A&&!(222<A)){var C=0,E=0;}else var E=1;if(E){var F=A,C=1;}}}if(!C)var F=A+32|0;w.safeSet(z,F);var G=z+1|0;if(y!==z){var z=G;continue;}break;}}var v=w;}return v;}var I=[0,0],J=null,N=undefined;function M(K){var L=K==J?0:[0,K];return L;}var O=RegExp,R=false,Q=Array;function S(P){return P instanceof Q?0:[0,new MlWrappedString(P.toString())];}I[1]=[0,S,I[1]];function U(T){return T;}var V=this.document;this.HTMLElement===N;function Z(W,X){var Y=W.toString();return X.tagName.toLowerCase()===Y?U(X):J;}var af=caml_js_get_console(0);function ae(_){return new O(caml_js_from_byte_string(_),p.toString());}function ag($,ab,aa){$.lastIndex=aa;var ac=$.exec(caml_js_from_byte_string(ab)),ad=ac==J?J:[0,ac.index,ac];return M(ad);}new O(n.toString(),o.toString());ae(m);function ak(ah){return ah.toString();}function an(ai){var aj=V.getElementById(ai.toString());if(aj==J){af.log(ak(ai));throw [0,a,i];}return aj;}function ao(al){var am=M(al);if(am)return am[1];throw [0,a,j];}var ap=ao(Z(r,an(h))),aq=ao(Z(q,an(g))),aw=0,ay=2;function aH(aG){var ar=aq.value,as=1,at=ap.rows.length;if(!(at<as)){var au=as;for(;;){var av=ao(ap.rows.item(au)),ax=ao(av.cells.item(aw)),az=ao(av.cells.item(ay)),aA=H(new MlWrappedString(ax.innerHTML));if(0===ag(ae(H(new MlWrappedString(ar))),aA,0)){var aB=H(new MlWrappedString(az.innerHTML));if(0===ag(ae(H(new MlWrappedString(ar))),aB,0)){var aC=av.style;aC.display=ak(k);var aD=1;}else var aD=0;}else var aD=0;if(!aD){var aE=av.style;aE.display=ak(l);}var aF=au+1|0;if(at!==au){var au=aF;continue;}break;}}return R;}aq.onkeyup=U(caml_js_wrap_callback(function(aI){if(aI){var aJ=aH(aI);if(!(aJ|0))aI.preventDefault();return aJ;}var aK=event,aL=aH(aK);if(!(aL|0))aK.returnValue=aL;return aL;}));f(0);return;}());
