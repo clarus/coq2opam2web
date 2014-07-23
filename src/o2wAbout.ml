00fb6fe446dccd0139217483c3c2926a16d396e0
Guillaume Claret <dev@clarus.me>
2014-07-23 15:27:27 +0200

    Working search button

--- src/o2wAbout.ml
+++ src/o2wAbout.ml
@@ -2,7 +2,7 @@
 let to_html =
   <:html<
     <h1>About</h1>
-    <p><strong>Coq Eggs</strong> is the package repository for the <a href="http://coq.inria.fr/">Coq</a> proof system. It simplifies collaborative development of large proofs and safe pograms. The packages are managed by <a href="http://opam.ocamlpro.com/">OPAM</a>, the state-of-the-art package manager for <a href="http://ocaml.org/">OCaml</a>.</p>
+    <p><strong>Coq Eggs</strong> is the package repository for the <a href="http://coq.inria.fr/">Coq</a> proof system. It simplifies collaborative development of large proofs and safe programs. The packages are managed by <a href="http://opam.ocamlpro.com/">OPAM</a>, the state-of-the-art package manager for <a href="http://ocaml.org/">OCaml</a>.</p>
     <p>For any request, please write at <a href="mailto:(λx.x@eggs.org)contact">(λx.x@eggs.org) contact</a>.</p>
 
     <h3>Contributors</h3>
